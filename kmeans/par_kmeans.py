3f306ae0e94dd27ff6e4628bb71357cddd779816
Grégoire Desjonquères <greg@cegesoft.com>
2024-04-16 16:53:10 +0200

    v0

--- kmeans/par_kmeans.py
+++ kmeans/par_kmeans.py
@@ -1,13 +1,16 @@
 import numpy as np
 from mpi4py import MPI
 
-def kmeans(points, k, max_iterations=1, convergence_threshold=0.05):
+def kmeans(points, k, max_iterations=100, convergence_threshold=0.005, total_points=20):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
+    centroids = np.empty((k,2))
     if rank == 0: # Initialisation aléatoire des centroïdes
         centroids = points[np.random.choice(range(len(points)), k, replace=False)]
+    
+    comm.Bcast(centroids, root=0)
 
     previous_labels = None
 
@@ -16,16 +19,35 @@ def kmeans(points, k, max_iterations=1, convergence_threshold=0.05):
         distances = np.linalg.norm(points[:, np.newaxis] - centroids, axis=2)
         labels = np.argmin(distances, axis=1)
         
-        # Mise à jour des centroïdes
-        centroids = np.array([points[labels == i].mean(axis=0) for i in range(k)])
+        # Mise à jour des centroïdes locaux
+        local_sum_points = np.array([np.sum(points[labels == i], axis=0) for i in range(k)])
+        nombre_points = np.array([len(points[labels == i]) for i in range(k)])
+        #comm.Barrier()
+
+        global_nombre_points = np.zeros_like(nombre_points)
+        global_sum_points = np.zeros_like(local_sum_points)
+        comm.Allreduce(local_sum_points, global_sum_points, op=MPI.SUM)
+        comm.Allreduce(nombre_points, global_nombre_points, op=MPI.SUM)
+
+        centroids = [global_sum_points[i]/global_nombre_points[i] for i in range(k)]
 
         #clause de convergence
         if previous_labels is not None:
             num_changed = np.sum(labels != previous_labels)
-            if num_changed / len(points) < convergence_threshold:
-                print(f"CLAUSE SEUIL!!! à {iteration}")
+            num_changed_global = comm.allreduce(num_changed, op=MPI.SUM)
+            if num_changed_global / total_points < convergence_threshold:
+                if rank == 0:
+                    print(f"CLAUSE SEUIL!!! à {iteration}")
                 break
         
         previous_labels = np.copy(labels)
-          
+        
     return labels, centroids
+
+
+np.random.seed(0)
+points = np.random.rand(20, 2)
+
+print(kmeans(points, 3))
+print("bonjour")
+
