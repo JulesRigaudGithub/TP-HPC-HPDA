9c35fd28dee52528eb48bad889bc1542abc67049
Grégoire Desjonquères <greg@cegesoft.com>
2024-04-25 11:09:30 +0200

    scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mpirun -np 2 -map-by ppr:2:node:PE=4 -rank-by core python3 kmeans ../Data/META_DATA_S1.txt
+mpirun -np 4 -map-by ppr:2:node:PE=4 -rank-by core python3 kmeans ../Data/META_DATA_S1.txt
